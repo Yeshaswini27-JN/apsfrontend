d472ffec7708543898e3bf909de10922de2f136d
Yeshaswini27-JN <138666982+Yeshaswini27-JN@users.noreply.github.com>
2025-03-15 12:41:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
 
   async function handleClick() {
 
-      const response = await axios.post("http://localhost:3000/login", { username });
+      const response = await axios.post("https://apsbackend-aejk.onrender.com/login", { username });
       console.log(response.data);
   
   }
